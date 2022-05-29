e8cac6fc6aa16cf4f2df6061d0686d9583215fea
jagottsicher <jagottsicher@gmx.net>
2022-05-29 13:22:34 +0800

    added code for part e)

--- mathehelfer/mathehelfer.go
+++ mathehelfer/mathehelfer.go
@@ -1,6 +1,8 @@
-// ein Test, ob mathehelfer zu finden ist
+// mathehelfee ist eine Package, dass nützliche Funktionen für Mathe zur Verfügung stellt
 package mathehelfer
 
+// DieSummeVon liefert die Summe über beliebige Anzahl von Ganzzahlen
+// Dazu wird in einer range immer weiter aufaddiert
 func DieSummeVon(eingabeWerte ...int) int {
 
 	summe := 0
@@ -15,12 +17,16 @@ func zweiGanzzahlenAddieren(a, b int) int {
 	return a + b
 }
 
+// EineAndereArtSumme liefert die Summe über beliebige Anzahl von Ganzzahlen
+// Dazu wird in einer range immer eine Summe aus Rückgabewert aus einer Funktion
+// mit nur zwei Summanden aufaddiert
 func EineAndereArtSumme(eingabeWerte ...int) int {
 
 	var summe int
 	summe = eingabeWerte[0]
 
 	for _, v := range eingabeWerte[1:] {
+
 		summe = zweiGanzzahlenAddieren(summe, v)
 	}
 
