9005a108c4832244f048859d792243d43e785040
jagottsicher <jagottsicher@gmx.net>
2022-05-29 13:31:33 +0800

    added code for part f)

--- mathehelfer/mathehelfer_test.go
+++ mathehelfer/mathehelfer_test.go
@@ -1,6 +1,9 @@
 package mathehelfer
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func BenchmarkDieSummeVon(b *testing.B) {
 
@@ -22,4 +25,17 @@ func BenchmarkEineAndereArtSumme(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		EineAndereArtSumme(sliceOfInt...)
 	}
+}
+
+func ExampleDieSummeVon() {
+	sliceOfInt := []int{1, 2, 3}
+	fmt.Println("Summe:", DieSummeVon(sliceOfInt...))
+	// Output:
+	// Summe: 6
+}
+
+func ExampleEineAndereArtSumme() {
+	fmt.Println("Summe:", EineAndereArtSumme(1, 2, 3))
+	// Output:
+	// Summe: 6
 }
